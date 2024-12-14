07bf4609e05a861645a6f13399647b5456df1b07
Corey Audrict <coreyaudrict@Mac.attlocal.net>
2024-12-14 12:50:22 -0600

    Testing Git skills

---
+++ Test1.java
@@ -0,0 +1,7 @@
+public class Test1 {
+
+        public static void main(String[] args){
+            System.out.println("Man I really am getting good at this. Keep going dawg!! You will be great!");
+        }
+    
+}
